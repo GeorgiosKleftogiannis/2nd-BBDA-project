800d84d4a40e50ea4fc1e86fa526fe5eee6c50b3
geokleftis@gmail.com <geokleftis@gmail.com>
2026-07-04 20:18:11 +0300

    final version of init.sql

---
+++ sql_scripts/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE weather_readings (
+    date TIMESTAMP,
+    temperature DOUBLE PRECISION,
+    relative_humidity DOUBLE PRECISION,
+    rain DOUBLE PRECISION,
+    showers DOUBLE PRECISION,
+    snowfall DOUBLE PRECISION,
+    precipitation DOUBLE PRECISION
+);
