2435e4ee2ef7b6a1ae3cf9a16acd7ba486c41f8a
geokleftis@gmail.com <geokleftis@gmail.com>
2026-07-04 12:22:31 +0300

    commit of the extract file for the weather of 3 cities

---
+++ src/extract.py
@@ -0,0 +1,59 @@
+import openmeteo_requests
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+pd.set_option('display.max_columns', None)
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": [38.25, 37.04, 39.67],
+	"longitude": [21.74, 22.11, 20.85],
+	"hourly": ["temperature_2m", "relative_humidity_2m", "rain", "showers", "snowfall", "precipitation", "weather_code", "pressure_msl"],
+}
+responses = openmeteo.weather_api(url, params = params)
+
+# Process 3 locations
+for response in responses:
+	print(f"\nCoordinates: {response.Latitude()}°N {response.Longitude()}°E")
+	print(f"Elevation: {response.Elevation()} m asl")
+	print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+	
+	# Process hourly data. The order of variables needs to be the same as requested.
+	hourly = response.Hourly()
+	hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+	hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+	hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+	hourly_showers = hourly.Variables(3).ValuesAsNumpy()
+	hourly_snowfall = hourly.Variables(4).ValuesAsNumpy()
+	hourly_precipitation = hourly.Variables(5).ValuesAsNumpy()
+	hourly_weather_code = hourly.Variables(6).ValuesAsNumpy()
+	hourly_pressure_msl = hourly.Variables(7).ValuesAsNumpy()
+	
+	hourly_data = {
+		"date": pd.date_range(
+			start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+			end =  pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+			freq = pd.Timedelta(seconds = hourly.Interval()),
+			inclusive = "left"
+		)
+	}
+	
+	hourly_data["temperature_2m"] = hourly_temperature_2m
+	hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+	hourly_data["rain"] = hourly_rain
+	hourly_data["showers"] = hourly_showers
+	hourly_data["snowfall"] = hourly_snowfall
+	hourly_data["precipitation"] = hourly_precipitation
+	hourly_data["weather_code"] = hourly_weather_code
+	hourly_data["pressure_msl"] = hourly_pressure_msl
+	
+	hourly_dataframe = pd.DataFrame(data = hourly_data)
+	print("\nHourly data\n", hourly_dataframe)
